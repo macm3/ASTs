6a0135ea5c11c44ce40c726b25e676e1246924b1
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-04-26 20:30:09 -0300

    Main class ready for testing project

--- hcmb_macm3/src/main/Main.java
+++ hcmb_macm3/src/main/Main.java
@@ -1,10 +1,27 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import ast.*;
+import visitor.PrettyPrintVisitor;
+
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		InputStream stream = new FileInputStream("src/teste.txt");
+		ANTLRInputStream input = new ANTLRInputStream(stream);
+		hcmb_macm3Lexer lexer = new hcmb_macm3Lexer(input);
+		CommonTokenStream token = new CommonTokenStream(lexer);
+		hcmb_macm3Parser parser = new hcmb_macm3Parser(token);
+		ParseTree tree = parser.goal();
+		Visitor visitor = new Visitor();
+		Program program = (Program) visitor.visit(tree);
+		program.accept(new PrettyPrintVisitor());
 	}
 
 }
--- hcmb_macm3/src/main/Visitor.java
+++ hcmb_macm3/src/main/Visitor.java
@@ -3,56 +3,10 @@ package main;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.plaf.synth.SynthSeparatorUI;
-
 import org.antlr.v4.runtime.tree.ParseTree;
+import ast.*;
 
-import ast.And;
-import ast.ArrayAssign;
-import ast.ArrayLength;
-import ast.ArrayLookup;
-import ast.Assign;
-import ast.Block;
-import ast.BooleanType;
-import ast.Call;
-import ast.ClassDecl;
-import ast.ClassDeclExtends;
-import ast.ClassDeclList;
-import ast.ClassDeclSimple;
-import ast.Exp;
-import ast.ExpList;
-import ast.False;
-import ast.Formal;
-import ast.FormalList;
-import ast.Identifier;
-import ast.IdentifierExp;
-import ast.IdentifierType;
-import ast.If;
-import ast.IntArrayType;
-import ast.IntegerLiteral;
-import ast.IntegerType;
-import ast.LessThan;
-import ast.MainClass;
-import ast.MethodDecl;
-import ast.MethodDeclList;
-import ast.Minus;
-import ast.NewArray;
-import ast.NewObject;
-import ast.Not;
-import ast.Plus;
-import ast.Print;
-import ast.Program;
-import ast.Statement;
-import ast.StatementList;
-import ast.This;
-import ast.Times;
-import ast.True;
-import ast.Type;
-import ast.VarDecl;
-import ast.VarDeclList;
-import ast.While;
-
-public class Visitor {
+public class Visitor extends hcmb_macm3BaseVisitor<Object>{
 	
 }
